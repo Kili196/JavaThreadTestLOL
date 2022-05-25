2c69071f30ba89a3cb9bd4eb70aba2deec86f81a
Kili196 <80536389+Kili196@users.noreply.github.com>
2022-05-25 16:39:32 +0200

    Update

--- src/MyFirstThread.java
+++ src/MyFirstThread.java
@@ -1,2 +1,10 @@
-public class MyFirstThread {
+public class MyFirstThread implements Runnable{
+
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 5; i++) {
+            System.out.println(i);
+        }
+    }
 }
