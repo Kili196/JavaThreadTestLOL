8896a4254e74639f261d67f04c431f28e055fa10
Kili196 <80536389+Kili196@users.noreply.github.com>
2022-05-25 23:59:20 +0200

    Update

--- src/MyFirstThread.java
+++ src/MyFirstThread.java
@@ -1,9 +1,43 @@
 public class MyFirstThread extends Thread{
+    private int threadNumber;
+
+    public MyFirstThread(int threadNumber) {
+        this.threadNumber = threadNumber;
+    }
 
     @Override
     public void run() {
         for (int i = 0; i < 5; i++) {
-            System.out.println(i);
+            System.out.println(i + "Number of Thread" + threadNumber);
+            try {
+                //Damit man sehen kann, das beide Threads gleichzeitig laufen!
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
+
+class MySecondThread extends Thread{
+
+    private int threadNumber;
+
+    public MySecondThread(int threadNumber) {
+        this.threadNumber = threadNumber;
+    }
+
+
+    @Override
+    public void run(){
+        for (int i = 0; i <= 5; i++) {
+            System.out.println(i + "Number of Thread" + threadNumber );
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+
         }
     }
 }
--- src/TestThread.java
+++ src/TestThread.java
@@ -1,8 +1,19 @@
 import com.sun.tools.javac.Main;
 
-public class TestThread {
+public class TestThread{
+    public static int amount = 0;
+
     public static void main(String[] args) {
-        TestThread myFirstThread = new TestThread();
+        MyFirstThread myFirstThread = new MyFirstThread(1);
+        myFirstThread.start();
+        MyFirstThread mySecondThread = new MyFirstThread(2);
+        mySecondThread.start();
+        MySecondThread mySecondThread1 = new MySecondThread(3);
+        mySecondThread1.start();
+
     }
+
+
+
 
 }
